3fc053295432e27d2cb56e5e7686ebe4407f4f94
Antoine Guy <aguy@viadeoteam.com>
2012-08-16 12:25:11 +0200

    add flow, map and outofsync capabilities

--- lib/puppet/type/flume_node.rb
+++ lib/puppet/type/flume_node.rb
@@ -21,25 +21,73 @@ Puppet::Type.newtype(:flume_node) do
     desc "master hostname"
   end
 
+  newparam(:flow) do
+    desc "Flow ID"
+    defaultto "default-flow"
+  end
+
+  newparam(:map) do
+    desc "Mapping to use"
+    defaultto {
+      nil
+    }
+  end
+
   newproperty(:ensure) do
     desc "Whether the resource is in sync or not."
 
     defaultto :insync
 
     def retrieve
-      `flume shell -q -c #{resource[:master]} -e getconfigs 2>/dev/null | grep #{resource[:name]} | grep -v null`
+      map = resource[:map]
+      if !map.nil? then
+        node = map
+      else
+        node = resource[:name]
+      end
+      `flume shell -q -c #{resource[:master]} -e getconfigs 2>/dev/null | grep #{node} | grep -v null`
       ($? == 0 ? :insync : :outofsync)
     end
 
     newvalue :outofsync do
-      # TODO
+      master = resource[:master]
+      name = resource[:name]
+      map = resource[:map]
+      
+      if !map.nil? then
+        mapping = <<-EOF
+exec unmap #{name} #{map}
+exec unconfig #{map}
+exec decommission #{map}
+exec purge #{map}
+EOF
+      end
+      conf = <<-EOF
+connect #{master}
+exec unconfig #{name}
+exec decommission #{name}
+exec purge #{name}
+EOF
+
+      Tempfile.open("flume-") do |tempfile|
+        tempfile.write(conf)
+        if defined? mapping
+          tempfile.write(mapping)
+        end
+        tempfile.close
+        `cat #{tempfile.path} | flume shell -q 2>/dev/null`
+      end
     end
+
     newvalue :insync do
 
       master = resource[:master]
       name = resource[:name]
       source = resource[:source]
       sink = resource[:sink]
+      flow = resource[:flow]
+      map = resource[:map]
+
       if sink.kind_of? Hash then
         # support hashes of the format:
         # { sinkType => [ array of nodes ] }
@@ -48,14 +96,31 @@ Puppet::Type.newtype(:flume_node) do
         sink = sink.keys.first + "( " + sink.values.first.shuffle.map{ |s| "\"#{s}\"" }.join(", ") + " )"
       end
 
-      conf = <<-EOF
+      if !map.nil? then
+        node = map
+        conf = <<-EOF
+connect #{master}
+exec unmap #{name} #{node}
+exec unconfig #{node}
+exec decommission #{node}
+exec purge #{node}
+exec decommission #{name}
+exec purge #{name}
+exec map #{name} #{node}
+exec config #{node} #{flow} '#{source}' '#{sink}'
+exec refresh #{node}
+EOF
+      else
+        node = name
+        conf = <<-EOF
 connect #{master}
 exec unconfig #{name}
 exec decommission #{name}
 exec purge #{name}
-exec config #{name} '#{source}' '#{sink}'
+exec config #{node} #{flow} '#{source}' '#{sink}'
 exec refresh #{name}
 EOF
+      end
 
       Tempfile.open("flume-") do |tempfile|
         tempfile.write(conf)
